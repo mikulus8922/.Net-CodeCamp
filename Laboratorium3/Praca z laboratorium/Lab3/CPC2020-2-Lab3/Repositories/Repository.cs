8f5270212d7ef11c16dac8f258a99ea7da5aaaba
mikulus8922 <mikulus8922@gmail.com>
2020-12-09 22:04:50 +0100

    dodanie zadania z zajęć

---
+++ Laboratorium3/Praca z laboratorium/Lab3/CPC2020-2-Lab3/Repositories/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace CPC2020_2_Lab3.Repositories
+{
+    /// <summary>
+    /// Klasa abstrakcyjna mająca zmienne i/lub metody, które każde repozytorium powinno zawierać
+    /// </summary>
+    public abstract class Repository
+    {
+        /// <summary>
+        /// Zmienna przechwująca połączenie z bazą danych
+        /// </summary>
+        protected readonly SqlConnection Connection;
+
+     
+        public Repository()
+        {
+            //Inicjalizacja połączenia z bazą danych
+            Connection = new SqlConnection(Properties.Resources.ConnectionString);
+        }
+    }
+}
