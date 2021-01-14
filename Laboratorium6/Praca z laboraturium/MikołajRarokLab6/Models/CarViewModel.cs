854b5f17f1560d03539de6eebc1ee61550bfff12
mikulus8922 <mikulus8922@gmail.com>
2021-01-14 03:13:35 +0100

    Dodano pracę z zajęć

---
+++ Laboratorium6/Praca z laboraturium/MikołajRarokLab6/Models/CarViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikołajRarokLab6.Models
+{
+    public class CarViewModel
+    {
+        /// <summary>
+        /// Konstruktor z parametrami
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="manufacturer"></param>
+        /// <param name="price"></param>
+        /// <param name="photoPath"></param>
+        public CarViewModel(string model, string manufacturer, decimal price, string photoPath)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.Price = price;
+            this.PhotoPath = photoPath;
+        }
+        
+
+        public string Model { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ Laboratorium6/Praca z laboraturium/MikołajRarokLab6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MikołajRarokLab6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
