00b6d3bb8c24ce2c3c25c67cedcfb26f6bb431cd
mikulus8922 <mikulus8922@gmail.com>
2021-01-20 21:21:23 +0100

    Dodano zadanie z labów

---
+++ Laboratorium7/Praca z laboratorium/MikołajRarokLab7/Controllers/PizzaController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MikołajRarokLab7.Models;
+using MikołajRarokLab7.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikołajRarokLab7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private IPizzaService _pizzaService;
+
+        
+        public PizzaController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        /// <summary>
+        /// Zwraca wszystkie pizze
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pizzas = _pizzaService.Get();
+            return Ok(pizzas);
+        }
+        
+        /// <summary>
+        /// Dodaje nową pizzę
+        /// </summary>
+        /// <param name="pizza"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post([FromBody]Pizza pizza)
+        {
+            int id = _pizzaService.Post(pizza);
+            return Ok(id);
+        }
+
+        /// <summary>
+        /// Edytuję istniejącą pizzę
+        /// </summary>
+        /// <param name="pizza"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromBody]Pizza pizza, [FromRoute]int id)
+        {
+            if(id != pizza.Id)
+            {
+                return Conflict("Podane Id są różne");
+            }
+            else
+            {
+                var isUpdateSuccessful = _pizzaService.Put(pizza, id);
+
+                if (isUpdateSuccessful)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute]int id)
+        {
+            var result = _pizzaService.Delete(id);
+
+            if (result)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Laboratorium7/Praca z laboratorium/MikołajRarokLab7/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikołajRarokLab7.Models
+{
+    public class Pizza
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public float Cost { get; set; }
+    }
+}
